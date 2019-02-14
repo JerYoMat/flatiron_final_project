c99166c9b780d84fad975583c243fc3d881785cc
JerYoMat <jeremy.emata@gmail.com>
2019-02-13 20:26:57 -0500

    added routes for models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-  resources :users
+  root   'static_pages#home'
+  get    '/about',   to: 'static_pages#about'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get '/auth/facebook/callback' => 'sessions#facebook_create'
+  get '/auth/github/callback' => 'sessions#github_create'
+  resources :units, :only [:show, :index]
+  resources :topics, :only [:show, :index]
+  resources :tips
+  resources :lessons, :only [:show, :index] do 
+    resources :tips 
+  end 
+  resources :users do 
+    resources :tips, only: [:index]
+  end 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
